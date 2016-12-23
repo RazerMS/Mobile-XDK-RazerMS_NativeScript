198d712f1b3da2136dd843dee255b27f7053414e
Leow <leow@Admins-MacBook-Pro.local>
2016-12-23 16:12:25 +0900

    update example project 3.7.0

--- MOLPayXDKExample/SimpleExample/node_modules/molpay-mobile-xdk-nativescript/MOLPayXDKExample/DetailsExample/platforms/ios/build/DetailsExample.build/Debug-iphonesimulator/DetailsExample.build/Objects-normal/i386/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/leow/Documents/NativeScript/DetailsExample/platforms/ios/internal/main.m \
-  /Users/leow/Documents/NativeScript/DetailsExample/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
-  /Users/leow/Documents/NativeScript/DetailsExample/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
-  /Users/leow/Documents/NativeScript/DetailsExample/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
-  /Users/leow/Documents/NativeScript/DetailsExample/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
-  /Users/leow/Documents/NativeScript/DetailsExample/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/leow/Documents/NativeScript/DetailsExample/platforms/ios/internal/TNSDebugging.h
