83ed3d224fed2765f7b713fc405cdd69542d4d25
Leow <lelouchliew93@hotmail.com>
2017-06-15 09:12:37 +0900

    Example 3.13.0

--- MOLPayXDKExample/SimpleExample/platforms/ios/build/SimpleExample.build/Debug-iphonesimulator/SimpleExample.build/Objects-normal/i386/main.d
+++ MOLPayXDKExample/SimpleExample/platforms/ios/build/SimpleExample.build/Debug-iphonesimulator/SimpleExample.build/Objects-normal/i386/main.d
@@ -1,8 +1,8 @@
 dependencies: \
-  /Users/leow/Documents/NativeScript/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/main.m \
-  /Users/leow/Documents/NativeScript/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
-  /Users/leow/Documents/NativeScript/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
-  /Users/leow/Documents/NativeScript/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
-  /Users/leow/Documents/NativeScript/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
-  /Users/leow/Documents/NativeScript/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/leow/Documents/NativeScript/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/TNSDebugging.h
+  /Volumes/Data/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/main.m \
+  /Volumes/Data/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Volumes/Data/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Volumes/Data/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Volumes/Data/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Volumes/Data/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/TNSExceptionHandler.h \
+  /Volumes/Data/molpay-mobile-xdk-nativescript/molpay-mobile-xdk-nativescript/MOLPayXDKExample/SimpleExample/platforms/ios/internal/TNSDebugging.h
